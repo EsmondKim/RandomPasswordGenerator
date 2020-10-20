4c9fd13580dcccf0d286594d99540dfbf8428ed4
EsmondKim <ekelectronicmail@gmail.com>
2020-10-19 23:23:28 -0500

    Fixed bug allowing users to select less than 8 or more than 128 characters, fixed commas appearing repeatedly in password as a randomly selected character.

--- script.js
+++ script.js
@@ -13,10 +13,17 @@ function writePassword() {
     let specialsPrompt = confirm("Do you want special characters in your password?")
     let lengthVariable = prompt("How long do you want your password to be? You MUST enter a numeric digit between 8 and 128.")
     //Creating the array pools for if statements and for loops to draw from.
-    let lowerCasePool = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-    let upperCasePool = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-    let numbersPool = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-    let specialsPool = [" ", "!", "\"", "#", "$", "%", "&", "\'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "-", "`", "{", "|", "}", "~"];
+    /*Declared Pool and PoolSplit variables on separate lines as separate variable to avoid JavaScript for some reason 
+    utilizing commas in final for loop that begins at line 101 when "abcdef".split(""); format was used on one line 
+    in one variable.*/ 
+    let lowerCasePool = "abcdefghijklmnopqrstuvwxyz";
+    let lowerCasePoolSplit = lowerCasePool.split(" ");
+    let upperCasePool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    let upperCasePoolSplit = upperCasePool.split(" ");
+    let numbersPool = "1234567890";
+    let numbersPoolSplit = numbersPool.split(" ");
+    let specialsPool = "\ !\"#$%&\',)*+-./\;:<=>?@[\\]^-`{|}~";
+    let specialsPoolSplit = specialsPool.split(" ");
     //Declaring a variable to build an array of selected criteria.  
     let criteriaArray = "";
     //Declaring the selection variables to be used in if statements and the for loop.
@@ -30,12 +37,25 @@ function writePassword() {
     let forLoopCounter = 0;
     //Declaring variable to build the password string.
     let passwordString = "";
-    //If statements build the criteriaArray and pull one index position each from every criteria selected.
-    confirm("Your password will be " + lengthVariable + " characters long.")
-
-       if(lowerCasePrompt === true) {
+    //If statement to confirm a numberic digit between 8 and 128 was entered by user.
+    if(lengthVariable < 8) {
+      alert("You MUST enter a numeric digit between 8 and 128, please hit Generate Password button and start again.");
+      passwordString = "Hit the Generate Password button and please be sure to enter a numeric digit between 8 and 128 for your password length."
+      return passwordString;
+      }
+    if(lengthVariable > 128) {
+      alert("You MUST enter a numeric digit between 8 and 128, please please hit Generate Password button and start again.");
+      passwordString = "Hit the Generate Password button and please be sure to enter a numeric digit between 8 and 128 for your password length."
+      return passwordString;      
+      }
+    else {
+      alert("Your password will be " + lengthVariable + " characters long.");
+    }
+      //If statements build the criteriaArray and pull one index position each from every criteria selected.  
+    if(lowerCasePrompt === true) {
       alert("Your password will include at least one lower case letter.");
-      criteriaArray = criteriaArray += lowerCasePool;
+      criteriaArray = criteriaArray += lowerCasePoolSplit;
+      //Tried to invoke criteriaInvoked++ iterater here, but elected for long form to avoid strange results.
       criteriaInvoked = criteriaInvoked + 1;
       let lowerCaseRandomizer = Math.floor(Math.random() * lowerCasePool.length);
       lowerCaseSelection = lowerCasePool[lowerCaseRandomizer];
@@ -45,7 +65,8 @@ function writePassword() {
 
     if(upperCasePrompt === true) {
       alert("Your password will include at least one upper case letter.");
-      criteriaArray = criteriaArray += upperCasePool;
+      criteriaArray = criteriaArray += upperCasePoolSplit;
+      //Tried to invoke criteriaInvoked++ iterater here, but elected for long form to avoid strange results.
       criteriaInvoked = criteriaInvoked + 1;
       let upperCaseRandomizer = Math.floor(Math.random() * upperCasePool.length);
       upperCaseSelection = upperCasePool[upperCaseRandomizer];
@@ -55,7 +76,8 @@ function writePassword() {
 
     if(numbersPrompt === true) {
       alert("Your password will include at least one number.");
-      criteriaArray = criteriaArray += numbersPool;
+      criteriaArray = criteriaArray += numbersPoolSplit;
+      //Tried to invoke criteriaInvoked++ iterater here, but elected for long form to avoid strange results.
       criteriaInvoked = criteriaInvoked + 1;
       let numbersRandomizer = Math.floor(Math.random() * numbersPool.length);
       numbersSelection = numbersPool[numbersRandomizer];
@@ -65,7 +87,8 @@ function writePassword() {
 
     if(specialsPrompt === true) {
       alert("Your password will include at least one special character.");
-      criteriaArray = criteriaArray += specialsPool;
+      criteriaArray = criteriaArray += specialsPoolSplit;
+      //Tried to invoke criteriaInvoked++ iterater here, but elected for long form to avoid strange results.
       criteriaInvoked = criteriaInvoked + 1;
       let specialsRandomizer = Math.floor(Math.random() * specialsPool.length);
       specialsSelection = specialsPool[specialsRandomizer];
@@ -77,7 +100,7 @@ function writePassword() {
     console.log(forLoopCounter);
     //For loop fills in rest of password drawing from combined pull of selected criteria.
     for (i = 0; forLoopCounter > i; forLoopCounter--) {
-      let criteriaArrayRandomizer = Math.floor(Math.random() * 10);
+      let criteriaArrayRandomizer = Math.floor(Math.random() * criteriaArray.length);
       criteriaArraySelection = criteriaArray[criteriaArrayRandomizer];
       passwordString = passwordString += criteriaArraySelection;
       console.log(passwordString);
