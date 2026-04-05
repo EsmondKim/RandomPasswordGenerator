82b00c2586ac6e3529cc669fd4f051712e86fbf1
EsmondKim <ekelectronicmail@gmail.com>
2026-04-05 16:51:25 -0500

    FINALLY removed all 'dem prompts. Better late than never.

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ function writePassword() {
     document.getElementById("specials").checked === false
   ) {
     alert(
-      "You MUST choose at least one of the following password criteria: lowercase, uppercase, numbers, or special characters."
+      "You MUST choose at least one of the following password criteria: lowercase, uppercase, numbers, or special characters.",
     );
     return;
   }
@@ -52,12 +52,9 @@ function writePassword() {
   if (lengthVar.value > 128) {
     alert("You must enter a numeric digit between 8 and 128.");
     return;
-  } else {
-    alert("Your password will be " + lengthVar.value + " characters long.");
   }
 
   if (document.getElementById("lowers").checked === true) {
-    alert("Your password will include at least one lower case letter.");
     criteriaArray = criteriaArray += lowerCasePoolSplit;
     criteriaInvoked = criteriaInvoked + 1;
     let lowerCaseRandomizer = Math.floor(Math.random() * lowerCasePool.length);
@@ -65,7 +62,6 @@ function writePassword() {
     passwordString = passwordString += lowerCaseSelection;
   }
   if (document.getElementById("uppers").checked === true) {
-    alert("Your password will include at least one upper case letter.");
     criteriaArray = criteriaArray += upperCasePoolSplit;
     criteriaInvoked = criteriaInvoked + 1;
     let upperCaseRandomizer = Math.floor(Math.random() * upperCasePool.length);
@@ -73,7 +69,6 @@ function writePassword() {
     passwordString = passwordString += upperCaseSelection;
   }
   if (document.getElementById("numbers").checked === true) {
-    alert("Your password will include at least one number.");
     criteriaArray = criteriaArray += numbersPoolSplit;
     criteriaInvoked = criteriaInvoked + 1;
     let numbersRandomizer = Math.floor(Math.random() * numbersPool.length);
@@ -82,7 +77,6 @@ function writePassword() {
   }
 
   if (document.getElementById("specials").checked === true) {
-    alert("Your password will include at least one special character.");
     criteriaArray = criteriaArray += specialsPoolSplit;
     criteriaInvoked = criteriaInvoked + 1;
     let specialsRandomizer = Math.floor(Math.random() * specialsPool.length);
@@ -93,7 +87,7 @@ function writePassword() {
   forLoopCounter = lengthVar.value - criteriaInvoked;
   for (i = 0; forLoopCounter > i; forLoopCounter--) {
     let criteriaArrayRandomizer = Math.floor(
-      Math.random() * criteriaArray.length
+      Math.random() * criteriaArray.length,
     );
     criteriaArraySelection = criteriaArray[criteriaArrayRandomizer];
     passwordString = passwordString += criteriaArraySelection;
@@ -104,35 +98,3 @@ function writePassword() {
 }
 
 generatePasswordBtn.addEventListener("click", writePassword);
-
-lowersCheckbox.addEventListener("change", function lowersChange(checkbox) {
-  if (document.getElementById("lowers").checked) {
-    alert("Your password will include lowercase letters!");
-  } else {
-    alert("You've removed lowercase letters from your password!");
-  }
-});
-
-uppersCheckbox.addEventListener("change", function uppersChange(checkbox) {
-  if (document.getElementById("uppers").checked) {
-    alert("Your password will include uppercase letters!");
-  } else {
-    alert("You've removed uppercase letters from your password!");
-  }
-});
-
-numbersCheckbox.addEventListener("change", function numbersChange(checkbox) {
-  if (document.getElementById("numbers").checked) {
-    alert("Your password will include numbers!");
-  } else {
-    alert("You've removed numbers from your password!");
-  }
-});
-
-specialsCheckbox.addEventListener("change", function specialsChange(checkbox) {
-  if (document.getElementById("specials").checked) {
-    alert("Your password will include special characters!");
-  } else {
-    alert("You've removed special characters from your password!");
-  }
-});
